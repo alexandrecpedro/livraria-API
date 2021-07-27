fb317581525fbcfdb3501fb2d705f60eff37ba6e
Alexandre Pedro <alexandrecpedro@gmail.com>
2021-07-26 22:21:24 -0300

    Livraria - API

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,28 +1,33 @@
-const { Livros } = require('../models');
+const { Livros } = require('../models')
 
 const indexController = {
     index: async (req, res) => {
-        const livros = await Livros.findAll();
-        return res.status(200).json(livros);
+        const livros = await Livros.findAll()
+        return res.status(200).json(livros)
     },
     mostraLivro: async (req, res) => {
-        let { id } = req.params;
-        const livro = await Livros.findByPk(id);
-        return res.status(200).json(livro);
+        let { id } = req.params
+        const livro = await Livros.findByPk(id)
+        return res.status(200).json(livro)
     },
     criarLivro: async (req, res) => {
-        let {titulo, quantidade_paginas, autor, ano_lancamento, estoque} = req.body;
+        let {titulo, quantidade_paginas, autor, ano_lancamento, estoque} = req.body
         const livro = await Livros.create({titulo, quantidade_paginas, autor, ano_lancamento, estoque})
         
+        livro.titulo==null ? "": res.json("error")
+
         .then(res.status(200).json({
             status: 'OK',
             message: 'Cadastrado com sucesso!',
             livro: `${titulo}`
         }))
+        .catch((err)=>{
+            console.log(err)
+        })
     },
     atualizarLivro: async (req, res) => {
-        let { id } = req.params;
-        let {titulo, quantidade_paginas, autor, ano_lancamento, estoque} = req.body;
+        let { id } = req.params
+        let {titulo, quantidade_paginas, autor, ano_lancamento, estoque} = req.body
         const livro = await Livros.update({titulo, quantidade_paginas, autor, ano_lancamento, estoque}, {where: {id}})
         
         .then(res.status(200).json({
@@ -32,9 +37,9 @@ const indexController = {
         }))
     },
     deletarLivro: async (req, res) => {
-        let { id } = req.params;
-        const livro = await Livros.destroy({where: {id}});
-        return res.status(200).json(livro);
+        let { id } = req.params
+        const livro = await Livros.destroy({where: {id}})
+        return res.status(200).json(livro)
     }
 }
 
